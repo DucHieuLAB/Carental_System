aa43b9f47eea30df9f5c86d046c52aa858794deb
@hieunxhe140911 <hieunxhe140911@fpt.edu.vn>
2022-10-09 02:36:17 +0700

    Finish Create Driver

--- Create_Entity/src/main/java/com/example/create_entity/Repository/LicenseRepository.java
+++ Create_Entity/src/main/java/com/example/create_entity/Repository/LicenseRepository.java
@@ -1,9 +1,14 @@
 package com.example.create_entity.Repository;
 
-import com.example.create_entity.Entity.DriverEntity;
+import com.example.create_entity.Entity.LicenseTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LicenseRepository  extends JpaRepository<DriverEntity,Integer> {
+public interface LicenseRepository  extends JpaRepository<LicenseTypeEntity,Long> {
+
+    @Query(value = "SELECT * from carrental.license_types where license_name=?1 ", nativeQuery = true)
+    LicenseTypeEntity Get_License_By_Id(String license);
+
 }
